6637daa5167ede5316644e30f7cd73fb1121ea2d
AndreLuvetama <andreluares30@gmail.com>
2021-02-20 14:05:38 -0300

    Criação do AndPoint

--- curso/src/main/java/com/ramotetech/curso/resources/UserResource.java
+++ curso/src/main/java/com/ramotetech/curso/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ramotetech.curso.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +11,13 @@ import com.ramotetech.curso.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@GetMapping
 	public ResponseEntity<User> findAll(){
 		
 		User u = new User(1L, "Maria", "maria@hotmail.com" , "119073636", "1239383");
 		
 		return ResponseEntity.ok().body(u);
+		
 	}
 	
 }
