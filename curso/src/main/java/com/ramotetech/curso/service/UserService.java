a82fc433f59a293f4e468f5448a2d5bffaf25d3b
AndreLuvetama <andreluares30@gmail.com>
2021-02-20 15:44:55 -0300

    Service layer, component registration

---
+++ curso/src/main/java/com/ramotetech/curso/service/UserService.java
@@ -0,0 +1,27 @@
+package com.ramotetech.curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ramotetech.curso.entities.User;
+import com.ramotetech.curso.repositories.UserRepository;
+
+@Component // registra o componente do springBoot
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj  = repository.findById(id);		
+		return obj.get();
+	}
+}
