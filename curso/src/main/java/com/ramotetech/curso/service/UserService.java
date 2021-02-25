559d6227d5311cd1956887358f7efe36b08ebba9
AndreLuvetama <andreluares30@gmail.com>
2021-02-25 20:44:41 -0300

    Exception delete

--- curso/src/main/java/com/ramotetech/curso/service/UserService.java
+++ curso/src/main/java/com/ramotetech/curso/service/UserService.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 
@@ -36,7 +37,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);			
+		} catch (DataIntegrityViolationException e ) {
+			throw new ResourceNotFoundException(e.getMessage());	
+		}
 	}
 	
 	public User update(Long id, User obj) {
